69aa30beeb7b5a49c14df502b9a831d59f29c1ba
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-06-07 21:53:28 +0200

    Add top_titles() prototype

--- modul5_zadanie2.py
+++ modul5_zadanie2.py
@@ -86,12 +86,53 @@ def search(title):
 
 def generate_views():
     random_item = random.choice(library)
-    random_item.views = random_item.views + random.randrange(1, 100)
-    print(random_item.title, random_item.premiere_date, random_item.genre, random_item.views)
+    random_number = random.randrange(1, 100)
+    for i in range(random_number):
+        random_item.play()
 
 def generate_views_x_10():
     for i in range(10):
         generate_views()
+
+def top_titles(content_type):    
+
+
+    if content_type.lower() == 'movie':
+        list_1 = []
+
+        for elem in library:
+            is_series = isinstance(elem, TvSeries)
+            if is_series == False:
+                list_1.append(elem)
+
+        for i in range(3):
+
+            movie = list_1[0]
+
+            for number in list_1:
+                if number.views > movie.views:
+                    movie = number
+            print(f'top watched is {movie.title} with {movie.views} views')
+            
+
+
+    if content_type.lower() == 'tv series':
+        list_2 = []
+
+        for elem in library:
+            is_series = isinstance(elem, TvSeries)
+            if is_series == True:
+                list_2.append(elem)
+
+        for i in range(15):
+
+            tv_series = list_2[0]
+
+            for number in list_2:
+                if number.views > tv_series.views:
+                    tv_series = number
+            print(f'top watched is {tv_series.title} with {tv_series.views} views')
+            
 
 
 
@@ -102,8 +143,6 @@ random_series = TvSeries(title='Family Guy', premiere_date='31.01.1999', genre='
 random_series_2 = TvSeries(title='The Simpsons', premiere_date='17.12.1989', genre='Comedy', views=0, episode_number='01', season_number='01')
 random_series_3 = TvSeries(title='Black Mirror', premiere_date='04.12.2011', genre='Science Fiction', views=0, episode_number='01', season_number='01')
 
-print(random_movie)
-print(random_series)
 random_movie.add_to_library()
 random_movie_2.add_to_library()
 random_movie_3.add_to_library()
@@ -111,18 +150,30 @@ random_series.add_to_library()
 random_series_2.add_to_library()
 random_series_3.add_to_library()
 
-print(library)
-
-get_movies()
-get_series()
-
-random_series.play()
-random_movie.play()
-print(random_movie.views)
-print(random_series.views)
-
-search('Top GUN')
-
 generate_views_x_10()
 
-search('a long way down')
+print(random_movie.views)
+print(random_movie_2.views)
+print(random_movie_3.views)
+print(random_series.views)
+print(random_series_2.views)
+print(random_series_3.views)
+
+top_titles('tv series')
+
+'''
+if __name__ == "__main__":
+
+    print('Biblioteka filmów i seriali')
+    random_movie = Movies(title='Interstellar', premiere_date='07.11.2014', genre='Science Fiction', views=0)
+    random_movie_2 = Movies(title='A Long Way Down', premiere_date='21.03.2014', genre='Black Comedy', views=0)
+    random_movie_3 = Movies(title='Top Gun', premiere_date='12.05.1986', genre='Action', views = 0)
+    random_series = TvSeries(title='Family Guy', premiere_date='31.01.1999', genre='Comedy', views=0, episode_number='01', season_number='01')
+    random_series_2 = TvSeries(title='The Simpsons', premiere_date='17.12.1989', genre='Comedy', views=0, episode_number='01', season_number='01')
+    random_series_3 = TvSeries(title='Black Mirror', premiere_date='04.12.2011', genre='Science Fiction', views=0, episode_number='01', season_number='01')
+
+    generate_views_x_10()
+
+    print('Najpopularniejsze filmy i seriale {datetime.now()}')
+
+'''
